5f84ba0a8dc4d06ed2b6f41edf4815027f082205
Collin McDade <Collin.mcdade@mullenmail.com>
2017-11-28 14:11:02 -0700

    Seperate methods

--- GroupSorter/src/GroupSorter.java
+++ GroupSorter/src/GroupSorter.java
@@ -4,10 +4,18 @@ public class GroupSorter
 
 		public static void main(String[] args)
 			{
-			//how about that
-			//help
-			//hi
-			System.out.println("Hello.");
+				
 			}
-
+		public void reader()
+			{
+				
+			}
+		public void sorter()
+			{
+			
+			}
+		public void printer()
+			{
+			
+			}
 	}
