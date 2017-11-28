dd611f3be3e1c789afed7947f53d73b792cad4ec
Regan Archibald <regan.archibald@mullenmail.com>
2017-11-28 14:57:40 -0700

    made into branch reginald

--- GroupSorter/src/Printer.java
+++ GroupSorter/src/Printer.java
@@ -3,14 +3,11 @@ import java.io.*;
 
 public class Printer
 	{
-
 		public static void main(String[] args)
 			{
 				for(int q = 0; q < Sorter.names.length(); q++)
 					{
 						System.out.println(Sorter.names.get(q));
 					}
-
 			}
-
 	}
