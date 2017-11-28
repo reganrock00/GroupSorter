eb39c7b5867e846501157e773b4649e0d37ec2bc
Jack Paul <jackrpaul13@gmail.com>
2017-11-28 14:23:09 -0700

    remove methods

--- GroupSorter/src/GroupSorter.java
+++ GroupSorter/src/GroupSorter.java
@@ -1,23 +1,5 @@
 
 public class GroupSorter
 	{
-
-		public static void main(String[] args)
-			{
-			
-			}
-		
-		public void reader()
-			{
-				
-			}
-		public void sorter()
-			{
-			
-			}
-		public void printer()
-			{
-			
-			}
 		
 	}
