b0b4d8d598725958e8b748a83003c9d9c18d8faf
Regan Archibald <regan.archibald@mullenmail.com>
2017-11-28 14:37:12 -0700

    basic printer class 1

--- GroupSorter/src/Printer.java
+++ GroupSorter/src/Printer.java
@@ -1,10 +1,15 @@
+import java.util.*;
+import java.io.*;
 
 public class Printer
 	{
 
 		public static void main(String[] args)
 			{
-			
+				for(int q = 0; q < Sorter.names.length(); q++)
+					{
+						System.out.println(Sorter.names.get(q));
+					}
 
 			}
 
